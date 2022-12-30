d36d22be46e6b79c915feda74194f896abeadbc4
Vctcode <victorode01@gmail.com>
2022-12-30 03:13:36 +0100

    fix: Links to app download

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -10,12 +10,12 @@ import appStoreSvg from '../../assets/applestore.svg'
 export default function Button() {
     return (
         <div className="btn-container">
-            <a href="https://google.com">
-                <img src={playStoreSvg} alt="" />
+            <a href="https://play.google.com/store/apps/details?id=com.logx">
+                <img src={playStoreSvg} alt="playstore" />
             </a>
 
-            <a href="https://google.com">
-                <img src={appStoreSvg} alt="" />
+            <a href="https://apps.apple.com/ng/app/log-x-democratizing-logistics/id1633230801">
+                <img src={appStoreSvg} alt="appstore" />
             </a>
 
         </div>
