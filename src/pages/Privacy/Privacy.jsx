245d7a9a40c80de1896fbeaae42ae87448ad1457
Vctcode <victorode01@gmail.com>
2022-12-31 05:28:40 +0100

    Style: Privacy page styling

--- src/pages/Privacy/Privacy.jsx
+++ src/pages/Privacy/Privacy.jsx
@@ -1,11 +1,14 @@
 import Banner from "../../components/Banner/Banner";
 
+// Style
+import pr from "./Privacy.module.css"
+
 export default function Privacy() {
     return (
         <>
             <Banner title="Privacy Policy" />
 
-            <div>
+            <div className={pr.privacyPage}>
                 <p>
                     Log-x Technology Ltd built the Log-x app as a Free app. This SERVICE is provided by Log-x Technology Ltd at no cost and is intended for use as is.
                     This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.
@@ -17,95 +20,96 @@ export default function Privacy() {
                     For the avoidance of doubt, the term the Software and the Application herein shall include its respective components, processes and design in its entirety.
 
                 </p>
-            </div>
 
-            <div>
-                <h4>Location Tracking</h4>
-                <p>
-                    To use or access our app seamlessly, you must permit the Log-x app  to access location services through the permission system used by your mobile operating system (“Platform”) or browser. We may collect the precise location of your device when the Log-x app is running in the foreground or background of your device. We may also derive your approximate location from your IP address. We use your location information to verify that you are present in your preferred region or city when you begin or engage in a delivery through the Log-x app, connect you with delivery opportunities in your zone, and track the progress and completion of your Deliveries. You can enable the location tracking feature through the settings on your device or Platform or when prompted by the Log-x mobile app. If you choose to disable the location feature through the settings on your device or Platform, Log-x app will not receive precise location information from your device, which will prevent you from being able to Trip and receiving delivery opportunities in your area.
-                </p>
-            </div>
 
-            <div>
-                <h4>Payment</h4>
-                <p>
-                    You may choose to pay for the services by cash only where available, by using the Log-x app as detailed below. Once you have used the Service, you are required to make payment in full to the rider/delivery personnel and such payment is non-refundable except in exceptional situations. If you have any complaints in relation to the delivery service provided, then that dispute must be taken up with the third-party rider/delivery personnel provider directly. We only promise to refund some percentages of your initial fund where there is unreasonable delay in delivery.
-                    You have to pay the riders/delivery personnel upfront through the option provided in the app. Failure to do so will result in the rider/delivery personnel cancelling your request. You shall not ask riders to receive cash payment directly, you can only make payment through the options provided on the Log-x app.
-                    Selecting a particular payment method means you are agreeing to the terms of service of the Company’s processing partners and your financial institution. You will bear all fees that may be charged by such processing partners and/or your financial institution (if any) for the payment method you have selected.
-                    Cash deposited on your dashboard are not redeemable for cash are non refundable except in the instant delivery was not carried out or completed and partly refundable where there is an unreasonable delay in delivery without your consent. You will not receive interest or other earnings on your cash deposited on the Log-x app. The Company may receive interest on amounts that the Company holds on your behalf. You agree to assign your rights to the Company for any interest derived from your cash. You should ensure that you have sufficient cash to pay for the delivery services. If you have insufficient credit balance to pay for the delivery services, you may deposit additional cash so that you may complete your payment for the delivery services. You may check your dashboard for cash balance in the Application.
+                <div>
+                    <h4>Location Tracking</h4>
+                    <p>
+                        To use or access our app seamlessly, you must permit the Log-x app  to access location services through the permission system used by your mobile operating system (“Platform”) or browser. We may collect the precise location of your device when the Log-x app is running in the foreground or background of your device. We may also derive your approximate location from your IP address. We use your location information to verify that you are present in your preferred region or city when you begin or engage in a delivery through the Log-x app, connect you with delivery opportunities in your zone, and track the progress and completion of your Deliveries. You can enable the location tracking feature through the settings on your device or Platform or when prompted by the Log-x mobile app. If you choose to disable the location feature through the settings on your device or Platform, Log-x app will not receive precise location information from your device, which will prevent you from being able to Trip and receiving delivery opportunities in your area.
+                    </p>
+                </div>
 
-                </p>
-            </div>
+                <div>
+                    <h4>Payment</h4>
+                    <p>
+                        You may choose to pay for the services by cash only where available, by using the Log-x app as detailed below. Once you have used the Service, you are required to make payment in full to the rider/delivery personnel and such payment is non-refundable except in exceptional situations. If you have any complaints in relation to the delivery service provided, then that dispute must be taken up with the third-party rider/delivery personnel provider directly. We only promise to refund some percentages of your initial fund where there is unreasonable delay in delivery.
+                        You have to pay the riders/delivery personnel upfront through the option provided in the app. Failure to do so will result in the rider/delivery personnel cancelling your request. You shall not ask riders to receive cash payment directly, you can only make payment through the options provided on the Log-x app.
+                        Selecting a particular payment method means you are agreeing to the terms of service of the Company’s processing partners and your financial institution. You will bear all fees that may be charged by such processing partners and/or your financial institution (if any) for the payment method you have selected.
+                        Cash deposited on your dashboard are not redeemable for cash are non refundable except in the instant delivery was not carried out or completed and partly refundable where there is an unreasonable delay in delivery without your consent. You will not receive interest or other earnings on your cash deposited on the Log-x app. The Company may receive interest on amounts that the Company holds on your behalf. You agree to assign your rights to the Company for any interest derived from your cash. You should ensure that you have sufficient cash to pay for the delivery services. If you have insufficient credit balance to pay for the delivery services, you may deposit additional cash so that you may complete your payment for the delivery services. You may check your dashboard for cash balance in the Application.
 
-            <div>
-                <h4>Taxes</h4>
-                <p>
-                    You agree that this Agreement shall be subject to all prevailing statutory taxes, duties, fees, charges and/or costs, however denominated, as may be in force in Nigeria and in connection with any future taxes that may be introduced at any point of time. You further agree to use your best efforts to do everything necessary and required by the relevant laws to enable, assist and/or defend the Company to claim or verify any input tax credit, set off, rebate or refund in respect of any taxes paid or payable in connection with the Service supplied under this Agreement.
-                </p>
-            </div>
+                    </p>
+                </div>
 
-            <div>
-                <h4>Information Collection and Use</h4>
-                <p>
-                    You agree that this Agreement shall be subject to all prevailing statutory taxes, duties, fees, charges and/or costs, however denominated, as may be in force in Nigeria and in connection with any future taxes that may be introduced at any point of time. You further agree to use your best efforts to do everything necessary and required by the relevant laws to enable, assist and/or defend the Company to claim or verify any input tax credit, set off, rebate or refund in respect of any taxes paid or payable in connection with the Service supplied under this Agreement.
+                <div>
+                    <h4>Taxes</h4>
+                    <p>
+                        You agree that this Agreement shall be subject to all prevailing statutory taxes, duties, fees, charges and/or costs, however denominated, as may be in force in Nigeria and in connection with any future taxes that may be introduced at any point of time. You further agree to use your best efforts to do everything necessary and required by the relevant laws to enable, assist and/or defend the Company to claim or verify any input tax credit, set off, rebate or refund in respect of any taxes paid or payable in connection with the Service supplied under this Agreement.
+                    </p>
+                </div>
 
-                    <ol>
-                        <li>To facilitate our Service;</li>
-                        <li>To provide for the Service on our behalf;</li>
-                        <li>To perform Service-related services; or
-                        </li>
-                        <li>To assist us in analyzing how our Service is used.
-                        </li>
-                    </ol>
+                <div>
+                    <h4>Information Collection and Use</h4>
+                    <p>
+                        You agree that this Agreement shall be subject to all prevailing statutory taxes, duties, fees, charges and/or costs, however denominated, as may be in force in Nigeria and in connection with any future taxes that may be introduced at any point of time. You further agree to use your best efforts to do everything necessary and required by the relevant laws to enable, assist and/or defend the Company to claim or verify any input tax credit, set off, rebate or refund in respect of any taxes paid or payable in connection with the Service supplied under this Agreement.
 
-                    We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
-                </p>
-            </div>
+                        <ol>
+                            <li>To facilitate our Service;</li>
+                            <li>To provide for the Service on our behalf;</li>
+                            <li>To perform Service-related services; or
+                            </li>
+                            <li>To assist us in analyzing how our Service is used.
+                            </li>
+                        </ol>
 
-            <div>
-                <h4>Log Data</h4>
-                <p>
-                    We want to inform you that whenever you use our Service, in the case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.
-                </p>
-            </div>
+                        We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
+                    </p>
+                </div>
 
-            <div>
-                <h4>Cookies</h4>
-                <p>
-                    Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
-                    This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
-                </p>
-            </div>
+                <div>
+                    <h4>Log Data</h4>
+                    <p>
+                        We want to inform you that whenever you use our Service, in the case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.
+                    </p>
+                </div>
 
-            <div>
-                <h4>Security</h4>
-                <p>
-                    We value your trust in providing us with your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.
+                <div>
+                    <h4>Cookies</h4>
+                    <p>
+                        Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
+                        This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
+                    </p>
+                </div>
 
-                </p>
-            </div>
+                <div>
+                    <h4>Security</h4>
+                    <p>
+                        We value your trust in providing us with your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.
 
-            <div>
-                <h4>Links to Other Sites</h4>
-                <p>
-                    This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
-                </p>
-            </div>
+                    </p>
+                </div>
 
-            <div>
-                <h4>Governing Law and Jurisdiction</h4>
-                <p>
-                    We want to provide the best service possible, taking this into consideration, where there is a dispute with regards to the service provided by Log-x Technology Limited, a complaint should be logged immediately with customer service email contact@logtechx.com.
-                    Log-x Technology Limited is a company registered under the Laws of the Federation of Nigeria and as such all disputes arising from the terms of service of the company will be heard by the courts established in the Constitution of  the Federal Republic of Nigeria.
+                <div>
+                    <h4>Links to Other Sites</h4>
+                    <p>
+                        This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
+                    </p>
+                </div>
 
-                </p>
-            </div>
+                <div>
+                    <h4>Governing Law and Jurisdiction</h4>
+                    <p>
+                        We want to provide the best service possible, taking this into consideration, where there is a dispute with regards to the service provided by Log-x Technology Limited, a complaint should be logged immediately with customer service email contact@logtechx.com.
+                        Log-x Technology Limited is a company registered under the Laws of the Federation of Nigeria and as such all disputes arising from the terms of service of the company will be heard by the courts established in the Constitution of  the Federal Republic of Nigeria.
 
-            <div>
-                <h4>Termination</h4>
-                <p>
-                    On becoming aware of any violation of these terms, Log-x Technology Limited reserve the right (but not obligated) to determine when your action or use of the app complies with these terms and may close your account and terminate user access from using the app for further transactions at any time without prior notice and with it sole discretion.
-                </p>
+                    </p>
+                </div>
+
+                <div>
+                    <h4>Termination</h4>
+                    <p>
+                        On becoming aware of any violation of these terms, Log-x Technology Limited reserve the right (but not obligated) to determine when your action or use of the app complies with these terms and may close your account and terminate user access from using the app for further transactions at any time without prior notice and with it sole discretion.
+                    </p>
+                </div>
             </div>
         </>
     )
