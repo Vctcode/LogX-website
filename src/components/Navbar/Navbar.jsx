4571d30d29a967da78cd1c182e80c1ac8fdbfb0f
Vctcode <victorode01@gmail.com>
2022-12-31 04:59:14 +0100

    Fix: Redirect on server

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ import Logo from '../../assets/LogX.svg'
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     const [isActiveBar, setIsActiveBar] = useState(false);
-    const [activeLink, setActiveLink] = useState(false);
+    // const [activeLink, setActiveLink] = useState(false);
 
     const navHandler = () => {
         setIsActiveBar(!isActiveBar)
