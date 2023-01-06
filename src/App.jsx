933caf0a19e2974e289fdcf9faa99c279294a244
Vctcode <victorode01@gmail.com>
2023-01-06 04:38:52 +0100

    Feat: Button linked to Whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import { FaWhatsapp as Whatsapp } from 'react-icons/fa'
+
+
 //  Components
 import Navbar from './components/Navbar/Navbar'
 import LearnHow from './pages/LearnHow/LearnHow'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home/Home'
 
+import "./index.css"
+
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
+    <IconContext.Provider value={{ color: '#fff', size: '24px', }}>
+      <div className="App">
+        {/* <Navbar /> */}
 
-      <Router>
-        <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/learnhow' exact element={<LearnHow />} />
-        </Routes>
-        {/* <SectionTwo /> */}
-      </Router>
+        <Router>
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path='/learnhow' exact element={<LearnHow />} />
+          </Routes>
+          {/* <SectionTwo /> */}
+        </Router>
 
-      <Footer />
-    </div>
+        {/* <Footer /> */}
+      </div>
+    </IconContext.Provider>
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { IconContext } from 'react-icons'
+import { SiWhatsapp as WhatsappIcon } from 'react-icons/si'
+
 import './index.css'
 import Home from './pages/LandingPage/Home'
 import Demo from './pages/Demo/Demo'
@@ -14,22 +17,27 @@ import AboutUs from './pages/AboutUs/AboutUs'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-      <ScrollToTop />
+    <IconContext.Provider value={{ color: '#fff', size: '34px', }}>
 
-      <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path={'/demo'} exact element={<Demo />} />
-        <Route path='/faq' exact element={<Faq />} />
-        <Route path='/privacy-policy' exact element={<Privacy />} />
-        <Route path='/about-us' exact element={<AboutUs />} />
-      </Routes>
-      {/* <SectionTwo /> */}
+      <Router>
+        <Navbar />
+        <ScrollToTop />
+
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path={'/demo'} exact element={<Demo />} />
+          <Route path='/faq' exact element={<Faq />} />
+          <Route path='/privacy-policy' exact element={<Privacy />} />
+          <Route path='/about-us' exact element={<AboutUs />} />
+        </Routes>
+        {/* <SectionTwo /> */}
 
 
+        <a href="https://api.whatsapp.com/send?phone=7013423935&text=Hello,%20I%20need%20a%20dispatch%20rider" className='whatsapp' ><WhatsappIcon /></a>
 
-      <Footer />
-    </Router>
+        <Footer />
+      </Router>
+    </IconContext.Provider>
+
   </React.StrictMode>
 )
